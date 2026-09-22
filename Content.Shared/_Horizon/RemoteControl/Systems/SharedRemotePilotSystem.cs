8d7e854b17a735324a965733672faadf933a865f
Timur <timur.garaev2007@gmail.com>
2026-09-23 01:05:48 +0300

    feat: Remote pilot inheriting faction from controller

--- Content.Shared/_Horizon/RemoteControl/Systems/SharedRemotePilotSystem.cs
+++ Content.Shared/_Horizon/RemoteControl/Systems/SharedRemotePilotSystem.cs
@@ -4,6 +4,8 @@ using System.Diagnostics.CodeAnalysis;
 using Content.Shared.DoAfter;
 using Content.Shared.Mech.EntitySystems;
 using Content.Shared.Popups;
+using Content.Shared.NPC.Systems;
+using Content.Shared.NPC.Components;
 
 namespace Content.Shared._Horizon.RemoteControl.Systems;
 
@@ -13,7 +15,7 @@ public abstract class SharedRemotePilotSystem : EntitySystem
     [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly RemoteControlSystem _remoteControlSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
-
+    [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -33,6 +35,12 @@ public abstract class SharedRemotePilotSystem : EntitySystem
             return false;
 
         pilotUid = PredictedSpawnAtPosition(hostComp.RemotePilot, Transform(mech).Coordinates);
+
+        if (TryComp<NpcFactionMemberComponent>(controller, out var faction))
+        {
+            EnsureComp<NpcFactionMemberComponent>(pilotUid.Value, out var pilotfaction);
+            _npcFaction.AddFactions((pilotUid.Value, pilotfaction), faction.Factions);
+        }
 
         //Don't create a pilot in the mech immediately, because we need to call the MechEntryEvent to initialize the UI update for controlling the mech.
         _doAfterSystem.TryStartDoAfter(new DoAfterArgs(EntityManager, pilotUid.Value, 0f, new MechEntryEvent(), mech, target: mech)
