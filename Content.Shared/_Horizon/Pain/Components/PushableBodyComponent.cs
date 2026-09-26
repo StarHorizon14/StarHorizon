eb896fbb7ac0e74df53489c7c73ef9fc086062cd
Astrolaris <>
2026-09-26 16:43:22 +0200

    remove: "убрана боль"

--- Content.Shared/_Horizon/Pain/Components/PushableBodyComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Shared._Horizon.Pain.Components;
-
-/// <summary>
-/// This is used for...
-/// </summary>
-[RegisterComponent]
-public sealed partial class PushableBodyComponent : Component;
--- Resources/Locale/ru-RU/_Horizon/pain.ftl
+++
@@ -1,2 +0,0 @@
-wieldable-component-pain-blocked = Вам слишком больно для стрельбы с двух рук
-pain-try-stand-up = Вам слишком больно, чтобы стоять
